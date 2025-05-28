49012f8648f661c8c70d5f055da11afe101a9a9d
Michael Monaghan <michael@monaghan.nyc>
2025-05-28 14:44:38 -0400

    Add Full Day 1 solution

--- 2017/Days/1/solution.test.ts
+++ 2017/Days/1/solution.test.ts
@@ -1,32 +1,69 @@
 import { describe, expect, it } from '@jest/globals';
-import InverseCaptcha2Pointer from './solution';
+import { InverseCaptcha2Pointer, InverseCaptchaPart2 } from './solution';
 
-describe('Day 1 - Example Tests', () => {
+describe('Day 1 - Part 1 - Testing the examples...', () => {
   it('1122 produces sum of 3', () => {
     const input = "1122";
     const result = InverseCaptcha2Pointer(input);
 
-    expect(result).toBe(3);
+    expect(result).toEqual(3);
   });
 
   it('1111 produces sum of 4', () => {
     const input = "1111";
     const result = InverseCaptcha2Pointer(input);
 
-    expect(result).toBe(4);
+    expect(result).toEqual(4);
   });
 
   it('1234 produces sum of 0', () => {
     const input = "1234";
     const result = InverseCaptcha2Pointer(input);
 
-    expect(result).toBe(0);
+    expect(result).toEqual(0);
   });
 
   it('91212129 produces sum of 9', () => {
     const input = "91212129";
     const result = InverseCaptcha2Pointer(input);
 
-    expect(result).toBe(9);
+    expect(result).toEqual(9);
+  });
+});
+
+describe("Day 1 - Part 2 - Testing the examples...", () => {
+  test('1212 produces sum of 6', () => {
+    const input = "1212";
+    const result = InverseCaptchaPart2(input);
+
+    expect(result).toEqual(6);
+  });
+
+  test('1221 produces sum of 0', () => {
+    const input = "1221";
+    const result = InverseCaptchaPart2(input);
+
+    expect(result).toEqual(0);
+  });
+
+  test('123425 produces sum of 4', () => {
+    const input = "123425";
+    const result = InverseCaptchaPart2(input);
+
+    expect(result).toEqual(4);
+  });
+
+  test('123123 produces sum of 12', () => {
+    const input = "123123";
+    const result = InverseCaptchaPart2(input);
+
+    expect(result).toEqual(12);
+  });
+
+  test('12131415 produces sum of 4', () => {
+    const input = "12131415";
+    const result = InverseCaptchaPart2(input);
+
+    expect(result).toEqual(4);
   });
 })
--- 2017/Days/1/solution.ts
+++ 2017/Days/1/solution.ts
@@ -6,8 +6,7 @@ const input = loadInput(PATH);
 /**
  * Find the sum of all digits that match the next digit in the list
  */
-
-export default function InverseCaptcha2Pointer(input: string): number {
+export function InverseCaptcha2Pointer(input: string): number {
   if (input.length <= 1) return 0;
 
   let sum = 0; // running sum
@@ -31,6 +30,32 @@ export default function InverseCaptcha2Pointer(input: string): number {
   return sum;
 }
 
+export function InverseCaptchaPart2(input: string): number {
+  let sum = 0;
 
+  let p1 = 0;
+  let p2 = (input.length / 2);
 
-console.log(InverseCaptcha2Pointer(input));``
+  while (p1 < input.length) {
+    console.count();
+    console.group("Looping")
+    console.log("p1:", p1, " / p2:", p2)
+    console.log("inp[p1]:", input[p1], " / input[p2]:", input[p2]);
+    console.log("Sum:", sum);
+    if (input[p1] == input[p2]) {
+      sum += Number(input[p1]);
+    }
+
+    if (p2 == input.length - 1) {
+      p2 = 0;
+      p1++;
+    } else {
+      p1++, p2++;
+    }
+    console.groupEnd();
+  }
+
+  return sum;
+}
+
+console.log(InverseCaptchaPart2(input));
