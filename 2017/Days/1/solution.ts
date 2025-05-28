1ab03d7c15926e17f9989e304db4f829a0eff3c1
Michael Monaghan <michael@monaghan.nyc>
2025-05-28 13:07:35 -0400

    Init

---
+++ 2017/Days/1/solution.ts
@@ -0,0 +1,34 @@
+import loadInput from "../../../utils";
+
+
+const input = loadInput();
+
+/**
+ * Find the sum of all digits that match the next digit in the list
+ */
+
+function InverseCaptcha2Pointer(input: string): number {
+  if (input.length <= 1) return 0;
+
+  let sum = 0; // running sum
+
+  let p1 = 0;
+  let p2 = 1;
+
+  while (p1 < input.length) {
+    if (input[p1] == input[p2]) {
+      sum += Number(input[p1]);
+    }
+
+    if (p2 == input.length - 1) {
+      p2 = 0;
+      p1++;
+    } else {
+      p1++, p2++;
+    }
+  }
+
+  return sum;
+}
+
+console.log(InverseCaptcha2Pointer(input));
---
+++ utils/index.ts
@@ -0,0 +1,17 @@
+import { readFileSync as fsReadFileSync } from 'node:fs';
+import process from 'node:process';
+
+export default function loadInput(path: string = ""): string {
+  let inputPath;
+  if (path.length == 0) {
+    // get cwd, find input.txt and use that
+    const cwd = process.cwd();
+    inputPath = `${cwd}/Input.txt`;
+  } else {
+    inputPath = path;
+  }
+
+  const text = fsReadFileSync(inputPath, 'utf8')
+
+  return text;
+}
