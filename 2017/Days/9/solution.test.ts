085e1a5da5268bf6f295590a0dbdd5ddfcbd1c2b
Michael Monaghan <michael@monaghan.nyc>
2025-07-08 13:03:55 -0400

    Day 9 solution. Not pretty but not horrible

--- 2017/Days/9/solution.test.ts
+++ 2017/Days/9/solution.test.ts
@@ -1,11 +1,111 @@
 import { describe, expect, it } from '@jest/globals';
-// import {  } from './solution';
+import { Day9 } from './solution';
 
 describe('Day 9 - Part 1 - Example Tests', () => {
-  it('The largest value in the instructions is 1', () => {
-    const input = ``
-    // const result = (input);
+  it('Score of 1', () => {
+    const input = `{}`
+    const result = Day9(input);
 
-    // expect(result).toEqual(1);
+    expect(result.score).toEqual(1);
+  });
+
+  it('Score of 6', () => {
+    const input = `{{{}}}`
+    const result = Day9(input);
+
+    expect(result.score).toEqual(6);
+  });
+
+  it('Score of 5', () => {
+    const input = `{{},{}}`
+    const result = Day9(input);
+
+    expect(result.score).toEqual(5);
+  });
+
+  it('Score of 16', () => {
+    const input = `{{{},{},{{}}}}`
+    const result = Day9(input);
+
+    expect(result.score).toEqual(16);
+  });
+
+  it('Score of 1', () => {
+    const input = `{<a>,<a>,<a>,<a>}`
+    const result = Day9(input);
+
+    expect(result.score).toEqual(1);
+  });
+
+  it('Score of 9', () => {
+    const input = `{{<ab>},{<ab>},{<ab>},{<ab>}}`
+    const result = Day9(input);
+
+    expect(result.score).toEqual(9);
+  });
+
+  it('Score of 9', () => {
+    const input = `{{<!!>},{<!!>},{<!!>},{<!!>}}`
+    const result = Day9(input);
+
+    expect(result.score).toEqual(9);
+  });
+
+  it('Score of 3', () => {
+    const input = `{{<a!>},{<a!>},{<a!>},{<ab>}}`
+    const result = Day9(input);
+
+    expect(result.score).toEqual(3);
+  });
+});
+
+describe('Day 9 - Part 2 - Example Tests', () => {
+  it('0 chars', () => {
+    const input = `<>`
+    const result = Day9(input);
+
+    expect(result.garbage).toEqual(0);
+  });
+
+  it('17 chars', () => {
+    const input = `<random characters>`
+    const result = Day9(input);
+
+    expect(result.garbage).toEqual(17);
+  });
+
+  it('3 chars', () => {
+    const input = `<<<<>`
+    const result = Day9(input);
+
+    expect(result.garbage).toEqual(3);
+  });
+
+  it('2 chars', () => {
+    const input = `<{!>}>`
+    const result = Day9(input);
+
+    expect(result.garbage).toEqual(2);
+  });
+
+  it('0 chars', () => {
+    const input = `<!!>`
+    const result = Day9(input);
+
+    expect(result.garbage).toEqual(0);
+  });
+
+  it('0 chars', () => {
+    const input = `<!!!>>`
+    const result = Day9(input);
+
+    expect(result.garbage).toEqual(0);
+  });
+
+  it('10 chars', () => {
+    const input = `<{o"i!a,<{i<a>`
+    const result = Day9(input);
+
+    expect(result.garbage).toEqual(10);
   });
 });
--- 2017/Days/9/solution.ts
+++ 2017/Days/9/solution.ts
@@ -7,8 +7,58 @@ const raw = loadInput(PATH);
 // --- Day 9: Stream Processing ---
 // -----------------------------------------
 
-// --- Part 1 ---
+function hasChars(c: string) {
+  const ALLCHARS = ['{', '}', '<', '>', '!'];
 
-export function Day9Part1() {
-
+  return ALLCHARS.includes(c);
 }
+
+const CHARS = {
+  groupOpen: '{',
+  groupClose: '}',
+  garbageOpen: '<',
+  garbageClose: '>',
+  cancel: '!'
+}
+
+export function Day9(raw: string) {
+  const stream = raw.split("");
+  const scores: number[] = [];
+  const stack: string[] = [];
+  let garbage = false;
+  let peek = null;
+  let depth = 0;
+  let garbageCount = 0;
+
+  for (let i = 0; i < stream.length; i++) {
+    let cur = stream[i];
+    if (garbage == false) {
+      if (cur == '{') {
+        depth++;
+        stack.push(cur);
+        peek = cur;
+      } else if (cur == '}' && peek == '{') {
+        scores.push(depth);
+        depth--;
+        stack.pop();
+      } else if (cur == '<') {
+        garbage = true;
+      }
+    } else {
+      if (cur == '!') {
+        i++;
+      } else if (cur == '>') {
+        garbage = false;
+      } else {
+        garbageCount++;
+      }
+    }
+  }
+
+  return {
+    score: scores.reduce((a, b) => a + b, 0),
+    garbage: garbageCount
+  };
+}
+
+console.log(Day9(raw))
