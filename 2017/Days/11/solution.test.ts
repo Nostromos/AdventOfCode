9e896bf59e3fb636e1971d9bac8ea6fed3daf681
Michael Monaghan <michael@monaghan.nyc>
2025-07-08 15:22:02 -0400

    Start Day 11

---
+++ 2017/Days/11/solution.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from '@jest/globals';
+import { Day11 } from './solution';
+
+describe('Day 11 - Part 1 - Example Tests', () => {
+  it('', () => {
+    const input = ``
+    const result = Day11(input);
+
+    expect(result).toEqual(1);
+  });
+});
---
+++ 2017/Days/11/solution.ts
@@ -0,0 +1,12 @@
+import loadInput from "@/utils";
+
+const PATH = "2017/Days/11/Input.txt"; // Relative to project root
+const raw = loadInput(PATH);
+
+// -----------------------------------------
+// --- Day 11: Hex Edge ---
+// -----------------------------------------
+
+export function Day11() {
+  
+}
