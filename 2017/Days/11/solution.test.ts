80883fa756aa81f15cf0c71501fa66e8f3f1e9c3
Michael Monaghan <michael@monaghan.nyc>
2025-07-08 16:49:03 -0400

    Day 11 solution

--- 2017/Days/11/solution.test.ts
+++ 2017/Days/11/solution.test.ts
@@ -1,11 +1,32 @@
 import { describe, expect, it } from '@jest/globals';
-import { Day11 } from './solution';
+import { Day11Part1 } from './solution';
 
 describe('Day 11 - Part 1 - Example Tests', () => {
-  it('', () => {
-    const input = ``
-    const result = Day11(input);
+  it('3 steps', () => {
+    const input = `ne,ne,ne`.split(",")
+    const result = Day11Part1(input);
 
-    expect(result).toEqual(1);
+    expect(result).toEqual(3);
+  });
+
+  it('0 steps', () => {
+    const input = `ne,ne,sw,sw`.split(",")
+    const result = Day11Part1(input);
+
+    expect(result).toEqual(0);
+  });
+
+  it('2 steps', () => {
+    const input = `ne,ne,s,s`.split(",")
+    const result = Day11Part1(input);
+
+    expect(result).toEqual(2);
+  });
+
+  it('3 steps', () => {
+    const input = `se,sw,se,sw,sw`.split(",")
+    const result = Day11Part1(input);
+
+    expect(result).toEqual(3);
   });
 });
--- 2017/Days/11/solution.ts
+++ 2017/Days/11/solution.ts
@@ -1,12 +1,56 @@
 import loadInput from "@/utils";
 
 const PATH = "2017/Days/11/Input.txt"; // Relative to project root
-const raw = loadInput(PATH);
+const input = loadInput(PATH).split(",")
 
 // -----------------------------------------
 // --- Day 11: Hex Edge ---
 // -----------------------------------------
 
-export function Day11() {
-  
+const DIRECTIONS = {
+  n: { dx: 0, dy: 1, dz: -1 },
+  ne: { dx: 1, dy: 0, dz: -1 },
+  se: { dx: 1, dy: -1, dz: 0 },
+  s: { dx: 0, dy: -1, dz: 1 },
+  sw: { dx: -1, dy: 0, dz: 1 },
+  nw: { dx: -1, dy: 1, dz: 0 },
 }
+
+function transform(coords: number[], direction: string) {
+  let [x, y, z] = coords;
+  if (direction in DIRECTIONS) {
+    return [
+      x + DIRECTIONS[direction as keyof typeof DIRECTIONS].dx,
+      y + DIRECTIONS[direction as keyof typeof DIRECTIONS].dy,
+      z + DIRECTIONS[direction as keyof typeof DIRECTIONS].dz
+    ]
+  }
+  return coords;
+}
+
+export function Day11Part1(directions: string[]) {
+  let cur = [0, 0, 0];
+
+  for (let step of directions) {
+    cur = transform(cur, step)
+  }
+
+  const [x, y, z] = [Math.abs(cur[0]), Math.abs(cur[1]), Math.abs(cur[2])]
+  return (x + y + z) / 2
+}
+
+export function Day11Part2(directions: string[]) {
+  let cur = [0, 0, 0];
+  let max = 0;
+
+  for (let step of directions) {
+    cur = transform(cur, step)
+    const [x, y, z] = [Math.abs(cur[0]), Math.abs(cur[1]), Math.abs(cur[2])]
+    let dist = (x + y + z) / 2;
+    if (dist > max) max = dist;
+  }
+
+  return max;
+}
+
+console.log(Day11Part2(input));
