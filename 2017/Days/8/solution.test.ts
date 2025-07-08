cc8588c3c6a9fafe067b0e2d022421a6b10af879
Michael Monaghan <michael@monaghan.nyc>
2025-07-08 12:02:03 -0400

    Forgot to add tests for Day 8

--- 2017/Days/8/solution.test.ts
+++ 2017/Days/8/solution.test.ts
@@ -9,6 +9,18 @@ c dec -10 if a >= 1
 c inc -20 if c == 10`
     const result = Day8Part1(input);
 
-    expect(result).toEqual(1);
+    expect(result.endMax).toEqual(1);
+  });
+});
+
+describe('Day 8 - Part 2 - Example Tests', () => {
+  it('The largest value in the instructions is 1', () => {
+    const input = `b inc 5 if a > 1
+a inc 1 if b < 5
+c dec -10 if a >= 1
+c inc -20 if c == 10`
+    const result = Day8Part1(input);
+
+    expect(result.foreverMax).toEqual(10);
   });
 });
