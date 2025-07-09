9605c0f8608b00f16f0f26c87fa829490a42167a
Michael Monaghan <michael@monaghan.nyc>
2025-07-09 10:59:17 -0400

    Day 12 solve & cleanup

--- 2017/Days/12/solution.test.ts
+++ 2017/Days/12/solution.test.ts
@@ -1,11 +1,31 @@
 import { describe, expect, it } from '@jest/globals';
-import { Day12 } from './solution';
+import { Day12Part1, Day12Part2 } from './solution';
 
-describe('Day X - Part 1 - Example Tests', () => {
-  it('', () => {
-    const input = ``
-    const result = Day12(input);
+describe('Day 12 - Part 1 - Example Tests', () => {
+  it('Example group has 6', () => {
+    const input = `0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5`
+    const result = Day12Part1(input);
+    expect(result.groupCount).toEqual(6);
+  });
+});
 
-    expect(result).toEqual(1);
+describe('Day 12 - Part 2 - Example Tests', () => {
+  it('2 total groups', () => {
+    const input = `0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5`
+    const intermediate = Day12Part1(input)
+    const result = Day12Part2(intermediate.map);
+    expect(result).toEqual(2);
   });
 });
--- 2017/Days/12/solution.ts
+++ 2017/Days/12/solution.ts
@@ -7,6 +7,62 @@ const raw = loadInput(PATH);
 // --- Day 12: Digital Plumber ---
 // -----------------------------------------
 
-export function Day12() {
-  
+export function Day12Part1(rawInput: string) {
+  const input = rawInput.split("\n");
+  const map = new Map()
+
+  for (let line of input) {
+    const [program, pipes] = line.split(" <-> ")
+    const neighbors = pipes.split(", ").map((n => Number(n)))
+    const node = {
+      visited: false,
+      neighbors
+    }
+    map.set(Number(program), node)
+  }
+
+  let groupCount = 0;
+  let queue = [0];
+  while (queue.length > 0) {
+    let p = queue.shift()!
+    let cur = map.get(p);
+    map.delete(p)
+    if (cur) {
+      groupCount++;
+      cur.neighbors.forEach((n: number) => {
+        let neighbor = map.get(n);
+        if (neighbor !== undefined) queue.push(n)
+      })
+    }
+  }
+
+  return {
+    groupCount,
+    map
+  };
+}
+
+export function Day12Part2(map: any) {
+  let totalGroups = 1;
+
+  while (map.size > 0) {
+    totalGroups++;
+
+    let keys = map.keys()
+    let start = keys.next().value
+    let queue = [start]
+
+    while (queue.length > 0) {
+      let p = queue.shift();
+      let c = map.get(p!);
+      if (c) {
+        map.delete(p!)
+        c!.neighbors.forEach((n: number) => {
+          let neighbor = map.get(n)
+          if (neighbor !== undefined) queue.push(n)
+        })
+      }
+    }
+  }
+  return totalGroups;
 }
