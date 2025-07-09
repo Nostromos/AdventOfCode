3f4e4cbdee86977efd81dc98af11f38186e251f8
Michael Monaghan <michael@monaghan.nyc>
2025-07-09 09:36:49 -0400

    Starting Day 12

---
+++ 2017/Days/12/solution.ts
@@ -0,0 +1,12 @@
+import loadInput from "@/utils";
+
+const PATH = "2017/Days/12/Input.txt"; // Relative to project root
+const raw = loadInput(PATH);
+
+// -----------------------------------------
+// --- Day 12: Digital Plumber ---
+// -----------------------------------------
+
+export function Day12() {
+  
+}
