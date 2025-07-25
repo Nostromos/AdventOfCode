1eb34242ef5ade54a0487f0ce1d7210e65a3f401
Michael Monaghan <michael@monaghan.nyc>
2025-07-25 15:15:48 -0400

    Start 2023 Day 23

---
+++ 2023/Days/23/solution.ts
@@ -0,0 +1,17 @@
+import loadInput from "@/utils";
+
+const PATH = "2023/Days/23/Input.txt"; // Relative to project root
+const raw = loadInput(PATH);
+
+// -----------------------------------------
+// --- Day 23: A Long Walk ---
+// -----------------------------------------
+
+function Day23Part1(raw: string) {
+  
+}
+
+const start = performance.now()
+console.log(Day23Part1(raw))
+const end = performance.now()
+console.log(`Time taken:`, end - start, "ms")
